8224ed906f051d315aa9885338abe747698a9f7a
Usek777 <91222585+Usek777@users.noreply.github.com>
2023-11-23 21:25:54 +0300

    Add tower aim
    + Добавил наведение башен на объект

--- Assets/Scripts/Environment/Towers/DefenceTowers/DefenceTower.cs
+++ Assets/Scripts/Environment/Towers/DefenceTowers/DefenceTower.cs
@@ -1,8 +1,106 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class DefenceTower : Tower
 {
+
+    private GameObject currentTarget;
     
+    // Радиус обнаружения
+    private float AimRadius = 5;
+
+    // Скорость поворота башни
+    private float rotationSpeed = 70f;
+
+
+
+
+
+
+
+    private void FixedUpdate()
+    {
+        // Находим ближайший объект (в радиусе обнаружения) и наводимся на него
+        FindNearestEnemy();
+    }
+
+
+
+
+
+
+    // Находит ближайшего врага в области видимости башни
+    private void FindNearestEnemy()
+    {
+        if (currentTarget == null)
+        {
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+            if (enemies.Length > 0)
+            {
+                if (enemies.Length == 1) { currentTarget = enemies.First(); }
+                else
+                {
+                    (float distance, GameObject enemy) nearestEnemy = (float.MaxValue, null);
+
+                    foreach (GameObject enemy in enemies)
+                    {
+                        float newDist = CountDistance(enemy);
+
+                        if (newDist < AimRadius)
+                        {
+                            Debug.Log($"Дистанция до цели ({newDist}) в радиусе обнаружения ({AimRadius})");
+                            nearestEnemy = nearestEnemy.distance > newDist ? (newDist, enemy) : nearestEnemy;
+                        }
+                    }
+
+                    currentTarget = nearestEnemy.enemy;
+
+                    Debug.Log($"Имя ближайшего объекта = {currentTarget?.name ?? "null"}");
+                }
+            }
+        }
+        else if (currentTarget != null)
+        {
+            AimOn(currentTarget);
+        }
+    }
+
+
+
+
+
+
+    // Считает дистанцию до объекта, переданного в качестве параметра
+    private float CountDistance(GameObject targetObject)
+    {
+        Vector2 towerPosition = transform.position;
+        Vector2 enemyPosition = targetObject.transform.position;
+
+        // Рассчитываю расстояние между башней и объектом
+        float distance = Vector2.Distance(towerPosition, enemyPosition);
+
+        return distance;
+    }
+
+
+
+
+
+
+    // Наводит ось Y башни на цель
+    private void AimOn(GameObject target)
+    {
+        Vector3 targetPosition = target.transform.position;
+        Vector3 direction = targetPosition - transform.position;
+
+        // Получаем угол наведения при помощи LookRotation
+        Quaternion lookRotation = Quaternion.LookRotation(Vector3.forward, direction);
+
+        // Рассчитываем кратчайший путь вращения и получаем новое вращение
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
+    }
 }
