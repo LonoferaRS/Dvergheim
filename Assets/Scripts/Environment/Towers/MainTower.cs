dafdac95cd4300532174a775346ca975dbbeb104
Sergey <Sergeyyashonkov@bk.ru>
2023-11-24 06:42:19 +0300

    HealthBar and Waypoints
    Добавлен хп-бар, его функционал
    
    Оптимизированы путевые точки и их работа

--- Assets/Scripts/Environment/Towers/MainTower.cs
+++ Assets/Scripts/Environment/Towers/MainTower.cs
@@ -1,15 +1,71 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainTower : MonoBehaviour
 {
+    public int maxHealth = 10;
+    private int currentHealth;
+
+    public HealthBar healthBar;
+
+    private bool isGameOver = false;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void TakeDamage(int damage)
+    {
+        // Уменьшаем здоровье башни
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+
+        // Проверяем, если здоровье башни меньше или равно нулю, уничтожаем башню или выполняем другие действия
+        if (currentHealth <= 0)
+        {
+            GameOver();   // Здесь код если хп = 0
+        }
+    }
+
+    void GameOver()
+    {
+        isGameOver = true;
+
+        // Останавливаем игру 
+        Time.timeScale = 0f;
+    }
+
+
+
+
+
+
+    void UpdateHealthUI()
+    {
+        // Обновляем значение хп бара
+        healthBar.SetHealth(currentHealth);
+    }
+
+
     void OnTriggerEnter2D(Collider2D other)
     {
         // Проверка, что в триггер вошел объект с тегом "Enemy"
         if (other.CompareTag("Enemy"))
         {
-            Debug.Log("Enter");
             // Уничтожаем объект врага
             Destroy(other.gameObject);
         }
+
+        // Проверка, что в триггер вошел объект с тегом "Enemy"
+        if (other.CompareTag("Enemy"))
+        {
+            // Уменьшаем здоровье башни на 1
+            TakeDamage(1);
+        }
     }
 }
+
+
