43a2f131736e647ade1d958b7fee9b64cb20c373
Fayld <aleks3204932@mail.ru>
2023-11-27 21:21:19 +0300

    CameraBoundaries
    Границы камеры

--- Assets/Scripts/PlayerCam/PlayerCamera/PlayerCamera.cs
+++ Assets/Scripts/PlayerCam/PlayerCamera/PlayerCamera.cs
@@ -7,9 +7,9 @@ public class PlayerCamera : MonoBehaviour
     public float moveSpeed = 3f;
     public float zoomSpeed = 5f;
     public float multiplier = 0.0005f;
-
+    public float[] borders;
+    float coefx = 1.776f;
     private Camera mainCamera;
-
     private void Start()
     {
         mainCamera = GetComponent<Camera>();
@@ -32,8 +32,24 @@ public class PlayerCamera : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 moveDirection = new Vector3(horizontalInput, verticalInput, 0f) * moveSpeed * Time.deltaTime;
-        transform.position += moveDirection;
-
+        Vector3 newPosition = transform.position + moveDirection;
+        if (newPosition.x-mainCamera.orthographicSize*coefx < borders[0])
+        {
+            newPosition = new Vector3(borders[0]+ mainCamera.orthographicSize * coefx, newPosition.y, newPosition.z);
+        }
+        if (newPosition.x+ mainCamera.orthographicSize * coefx > borders[2])
+        {
+            newPosition = new Vector3(borders[2]- mainCamera.orthographicSize * coefx, newPosition.y, newPosition.z);
+        }
+        if (newPosition.y + mainCamera.orthographicSize > borders[1])
+        {
+            newPosition = new Vector3(newPosition.x, borders[1]- mainCamera.orthographicSize, newPosition.z);
+        }
+        if (newPosition.y - mainCamera.orthographicSize < borders[3])
+        {
+            newPosition = new Vector3(newPosition.x, borders[3] + mainCamera.orthographicSize, newPosition.z);
+        }
+        transform.position = newPosition;
         // Изменение масштаба камеры
         float scrollInput = Input.GetAxis("Mouse ScrollWheel");
 
